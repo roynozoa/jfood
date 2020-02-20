ed71498df20ef0df17aa8637f47d19af8741ebc4
unknown <muhammad.adisatriyo@ui.ac.id>
2020-02-20 17:07:22 +0700

    first commit

---
+++ Customer.java
@@ -0,0 +1,49 @@
+
+
+public class Customer {
+    private int id;
+    private String name,email, password, joidDate;
+    
+    public static int getId(int idCustomer){
+        return idCustomer;
+    }
+    
+    public static String getName(String nameCustomer){
+        return nameCustomer;
+    }
+    
+    public static String getEmail(String emailCustomer){
+        return emailCustomer;
+    }
+    
+    public static String getPassword(String passwordCustomer){
+        return passwordCustomer;
+    }
+    
+    public static String getJoinDate(String joinDateCustomer){
+        return joinDateCustomer;
+    }
+    
+    public static void setId(int id){
+        
+    }
+    
+    public static void setName(String name){
+    
+    }
+    
+    public static void setEmail(String email){
+    
+    }
+    
+    public static void setPassword(String password){
+    
+    }
+    
+    public static void setJoinDate(String joinDate){
+    
+    }
+    
+    
+
+}
---
+++ JFood.java
@@ -0,0 +1,8 @@
+
+public class JFood
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args){
+    
+    }
+}
