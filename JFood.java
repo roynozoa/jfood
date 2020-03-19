a39a4f0eb768720375c7487475529d2a428d050b
roynozoa <muhammad.adisatriyo@ui.ac.id>
2020-03-19 15:44:33 +0700

    test push

--- JFood.java
+++ JFood.java
@@ -1,26 +1,6 @@
 public class JFood
 {
     public static void main(String[] args){
-        //Soal CS
-        Location lokasi = new Location("Jakarta Timur", "DKI Jakarta", "Lokasi jajan gorengan");
-        Seller tukangGorengan = new Seller(003, "Muhammad Adisatriyo Pratama","muhammad.adisatriyo@ui.ac.id", "081297449833", lokasi);
-        Customer pembeli = new Customer(001, "Pembeli", "email@pembeli.com", "inipassword", "05-03-2020");
-        Promo promoCashback = new Promo(001, "Promo Cashback", 5000, 20000, true);
-        Promo promoCashback2 = new Promo(001, "Promo Cashback 2", 5000, 30000, true);
-        Food gorengan = new Food(002,"Gorengan",tukangGorengan, 25000, FoodCategory.Snacks);
-        Invoice struk = new CashlessInvoice(001, gorengan, "12/03/2020", pembeli, InvoiceStatus.Ongoing);
-        Invoice struk2 = new CashlessInvoice(002, gorengan, "12/03/2020", pembeli, InvoiceStatus.Ongoing, promoCashback2);
-        Invoice struk3 = new CashlessInvoice(003, gorengan, "12/03/2020", pembeli, InvoiceStatus.Ongoing, promoCashback);
-          
-        struk.printData();
-        struk2.printData();
-        struk3.printData();
-        
-        //Soal Post Test
-        Invoice strukTanpaOngkir = new CashInvoice(1, gorengan, "13/03/2020", pembeli, InvoiceStatus.Finished);
-        Invoice strukDenganOngkir = new CashInvoice(1, gorengan, "13/03/2020", pembeli, InvoiceStatus.Finished, 10000);
-        
-        strukTanpaOngkir.printData();
-        strukDenganOngkir.printData();
+        // MODUL 5 test push
     }
 }
